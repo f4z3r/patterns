c082aaa64938f218667658603824e9d8375549dd
Jakob Beckmann <beckmann_jakob@hotmail.fr>
2018-04-11 20:28:39 +0200

    Made modules public to allow doc generation for easier theory reading

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
-//! Main program
+//! A collection of design patterns.
 
 #![deny(missing_docs)]
 #![allow(dead_code)]
 
-mod factory_method;
-mod abstract_factory;
-mod prototype;
-mod singleton;
+pub mod factory_method;
+pub mod abstract_factory;
+pub mod prototype;
+pub mod singleton;
 
 fn main() {
 }
