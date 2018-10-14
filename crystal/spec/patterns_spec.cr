9b9836bbb6a728671cccbf7bc77976a84b4aa73b
Jakob Beckmann <beckmann_jakob@hotmail.fr>
2018-10-14 18:51:00 +0200

    added abstract factory pattern crystal

--- crystal/spec/patterns_spec.cr
+++ crystal/spec/patterns_spec.cr
@@ -1,10 +1,24 @@
 require "./spec_helper"
 
 describe Patterns do
-  # TODO: Write tests
-
   it "factory method works" do
     scary_maze = Patterns::ScaryMaze.new
     scary_maze.print.should eq("a very scarrrrryyy room;a very scarrrrryyy room;")
+  end
+
+  it "abstract factory works" do
+    os_type = "OSX"
+    factory = nil
+    case os_type
+    when "Windows"
+      factory = Patterns::WindowsFactory.new
+    when "OSX"
+      factory = Patterns::OSXFactory.new
+    end
+
+    unless factory.nil?
+      button = factory.create_button
+      button.paint.should eq("osx button")
+    end
   end
 end
---
+++ crystal/src/patterns/abstract_factory.cr
@@ -0,0 +1,41 @@
+# Abstract factory pattern
+
+module Patterns
+  # Abstract entity to be created by the factory.
+  abstract class Button
+    abstract def paint: String
+  end
+
+  # Interface of the factory. Defines the factory methods.
+  abstract class Factory
+    abstract def create_button: Button
+  end
+
+  # Concrete entity created by a factory.
+  class OSXButton < Button
+    def paint
+      "osx button"
+    end
+  end
+
+  # Concrete entity created by a factory.
+  class WindowsButton < Button
+    def paint
+      "windows button"
+    end
+  end
+
+  # Concrete factory creating objects.
+  class OSXFactory < Factory
+    def create_button
+      OSXButton.new
+    end
+  end
+
+  # Concrete factory creating objects.
+  class WindowsFactory < Factory
+    def create_button
+      WindowsButton.new
+    end
+  end
+end
