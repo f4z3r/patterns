be763635161fc56663f7d163b20b45023c459934
Jakob Beckmann <beckmann_jakob@hotmail.fr>
2018-10-17 17:56:11 +0200

    added command pattern crystal

--- crystal/spec/patterns_spec.cr
+++ crystal/spec/patterns_spec.cr
@@ -3,7 +3,7 @@ require "./spec_helper"
 describe Patterns do
   it "factory method works" do
     scary_maze = Patterns::ScaryMaze.new
-    scary_maze.print.should eq("a very scarrrrryyy room;a very scarrrrryyy room;")
+    scary_maze.print.should eq "a very scarrrrryyy room;a very scarrrrryyy room;"
   end
 
   it "abstract factory works" do
@@ -18,14 +18,14 @@ describe Patterns do
 
     unless factory.nil?
       button = factory.create_button
-      button.paint.should eq("osx button")
+      button.paint.should eq "osx button"
     end
   end
 
   it "decorator works" do
     coffee = Patterns::WithChocolate.new(Patterns::WithMilk.new(Patterns::SimpleCoffee.new))
-    coffee.cost.should eq(1.8)
-    coffee.ingredients.should eq("coffee, milk, chocolate")
+    coffee.cost.should eq 1.8
+    coffee.ingredients.should eq "coffee, milk, chocolate"
   end
 
   it "builder works" do
@@ -35,21 +35,31 @@ describe Patterns do
     builder.set_model("sedan")
     builder.set_colour("red")
     car = builder.construct
-    car.description.should eq("red sedan car with 7 seats and 4 doors.")
+    car.description.should eq "red sedan car with 7 seats and 4 doors."
   end
 
   it "state works" do
     post = Patterns::Post.new
     post.add_text("This is my amazing post.")
-    post.get_content.should eq("")
+    post.get_content.should eq ""
     post.request_review
-    post.get_content.should eq("")
+    post.get_content.should eq ""
     post.approve
-    post.get_content.should eq("This is my amazing post.")
+    post.get_content.should eq "This is my amazing post."
     post.add_text(" And I made some changed to make it even more awesome.")
-    post.get_content.should eq("")
+    post.get_content.should eq ""
     post.request_review
     post.approve
-    post.get_content.should eq("This is my amazing post. And I made some changed to make it even more awesome.")
+    post.get_content.should eq "This is my amazing post. And I made some changed to make it even more awesome."
+  end
+
+  it "command works" do
+    client = Patterns::Client.new
+    client.switch_for("on")
+    client.switch_for("off")
+    client.switch.history.should eq ["light turned on", "light turned off"]
+    expect_raises(ArgumentError, "unsupported argument 'else'") do
+      client.switch_for("else")
+    end
   end
 end
---
+++ crystal/src/patterns/command.cr
@@ -0,0 +1,68 @@
+# command design pattern
+
+module Patterns
+  # the command executor which stores a command history
+  class Switch
+    getter history : Array(String)
+
+    def initialize
+      @history = [] of String
+    end
+
+    def execute(command : Command)
+      @history << command.execute
+    end
+  end
+
+  # abstract command superclass
+  abstract class Command
+    getter obj : Light
+
+    def initialize(@obj)
+    end
+
+    abstract def execute
+  end
+
+  # concrete command
+  class TurnOnCommand < Command
+    def execute
+      @obj.turn_on
+    end
+  end
+
+  # other concrete command
+  class TurnOffCommand < Command
+    def execute
+      @obj.turn_off
+    end
+  end
+
+  # receiver of the command
+  class Light
+    def turn_on
+      "light turned on"
+    end
+
+    def turn_off
+      "light turned off"
+    end
+  end
+
+  class Client
+    getter switch : Switch
+
+    def initialize
+      @light = Light.new
+      @switch = Switch.new
+    end
+
+    def switch_for(cmd : String) : Nil
+      case cmd
+      when "on"   then @switch.execute(TurnOnCommand.new(@light))
+      when "off"  then @switch.execute(TurnOffCommand.new(@light))
+      else raise ArgumentError.new("unsupported argument '#{cmd}'")
+      end
+    end
+  end
+end
